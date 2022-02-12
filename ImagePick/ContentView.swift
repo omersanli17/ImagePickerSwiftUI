d76770e4c967dba6631eba2e68e2c85de98ccaf4
omer <sanli.omer@metu.edu.tr>
2022-02-12 15:34:24 +0300

    Initial Commit

---
+++ ImagePick/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImagePick
+//
+//  Created by omer sanli on 12.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImagePick/ImagePickApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePickApp.swift
+//  ImagePick
+//
+//  Created by omer sanli on 12.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ImagePickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
